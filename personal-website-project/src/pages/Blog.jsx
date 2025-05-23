273ae6444b29ca2c140255b5c0eceaf558e06ac4
Nail Valeev <nail.valeev2106@mail.ru>
2025-05-23 10:53:49 +0500

    pushing to repository

---
+++ personal-website-project/src/pages/Blog.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router"
+
+function Blog() {
+
+    return (
+        <>
+        <div>Blog page!</div>
+        <button><NavLink to="/">Go back</NavLink></button>
+        </>
+    )
+}
+
+export default Blog
---
+++ personal-website-project/src/pages/Videos.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router"
+
+function Videos() {
+
+    return (
+        <>
+        <div>Videos page!</div>
+        <button><NavLink to="/">Go back</NavLink></button>
+        </>
+    )
+}
+
+export default Videos
